03c14b12dc6758ed38af2e16d07df815772b1c4b
Stefan Magnus Landrø <stefan.landro@gmail.com>
2017-09-08 09:59:23 +0200

    Added methods for checking namespace existence and deleting namespaces

--- hcp.go
+++ hcp.go
@@ -215,6 +215,47 @@ func (hcp *HCP) CreateNamespace(namespace *Namespace) error {
 
 }
 
+func (hcp *HCP) DeleteNamespace(name string) error {
+	if req, reqErr := hcp.createDeleteRequest("/namespaces/" + name); reqErr != nil {
+		return reqErr
+	} else {
+
+		if res, doReqErr := hcp.getClient().Do(req); doReqErr != nil {
+			return doReqErr
+		} else {
+			if res.StatusCode != http.StatusOK {
+				return fmt.Errorf("Failed to delete HCP namespace: %s. Status code: %d, HCP error message: %s",
+					name,
+					res.StatusCode,
+					hcpErrorMessage(res))
+			} else {
+				return nil
+			}
+		}
+
+	}
+}
+
+func (hcp *HCP) NamespaceExists(name string) (bool, error) {
+	if req, reqErr := hcp.createHeadRequest("/namespaces/" + name); reqErr != nil {
+		return false, reqErr
+	} else {
+
+		if res, doReqErr := hcp.getClient().Do(req); doReqErr != nil {
+			return false, doReqErr
+		} else {
+			if res.StatusCode != http.StatusOK && res.StatusCode != http.StatusNotFound {
+				return false, fmt.Errorf("Failed to check existence of HCP namespace: %s. Status code: %d, HCP error message: %s",
+					name,
+					res.StatusCode,
+					hcpErrorMessage(res))
+			}
+			return res.StatusCode == http.StatusOK, nil
+		}
+
+	}
+}
+
 // VERBS
 
 func (hcp *HCP) createHeadRequest(urlStr string) (*http.Request, error) {
--- xml.go
+++ xml.go
@@ -65,7 +65,8 @@ const (
 
 // Optimized for
 const (
-	ALL = "ALL"
+	ALL   = "ALL"
+	CLOUD = "CLOUD"
 )
 
 // Hard quota units
@@ -79,24 +80,24 @@ type Namespace struct {
 	Name                          string             `xml:"name,omitempty"`
 	Description                   string             `xml:"description,omitempty"`
 	HashScheme                    string             `xml:"hashScheme,omitempty"`
-	EnterpriseMode                bool               `xml:"enterpriseMode,omitempty"`
+	EnterpriseMode                bool               `xml:"enterpriseMode"`
 	HardQuota                     string             `xml:"hardQuota,omitempty"`
 	SoftQuota                     int                `xml:"softQuota,omitempty"`
 	OptimizedFor                  string             `xml:"optimizedFor,omitempty"`
 	VersioningSettings            VersioningSettings `xml:"versioningSettings,omitempty"`
-	SearchEnabled                 bool               `xml:"searchEnabled,omitempty"`
-	IndexingEnabled               bool               `xml:"indexingEnabled,omitempty"`
-	CustomMetadataIndexingEnabled bool               `xml:"customMetadataIndexingEnabled,omitempty"`
-	ReplicationEnabled            bool               `xml:"replicationEnabled,omitempty"`
-	ReadFromReplica               bool               `xml:"readFromReplica,omitempty"`
-	ServiceRemoteSystemRequests   bool               `xml:"serviceRemoteSystemRequests,omitempty"`
+	SearchEnabled                 bool               `xml:"searchEnabled"`
+	IndexingEnabled               bool               `xml:"indexingEnabled"`
+	CustomMetadataIndexingEnabled bool               `xml:"customMetadataIndexingEnabled"`
+	ReplicationEnabled            bool               `xml:"replicationEnabled"`
+	ReadFromReplica               bool               `xml:"readFromReplica"`
+	ServiceRemoteSystemRequests   bool               `xml:"serviceRemoteSystemRequests"`
 	Tags                          []string           `xml:"tags>tag,omitempty"`
 }
 
 type VersioningSettings struct {
-	Enabled   bool `xml:"enabled,omitempty"`
-	Prune     bool `xml:"prune,omitempty"`
-	PruneDays int  `xml:"pruneDays,omitempty"`
+	Enabled   bool `xml:"enabled"`
+	Prune     bool `xml:"prune"`
+	PruneDays int  `xml:"pruneDays"`
 }
 
 func (ns *Namespace) marshal() ([]byte, error) {
